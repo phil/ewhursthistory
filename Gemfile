98c2f241be42f9e7d1b96b18ee3769c5021a7a0d
Phil Balchin <phil@maniacalrobot.co.uk>
2014-08-21 21:31:48 +0100

    Updates wwi, rack rewrite

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
 
+gem 'rack-rewrite'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails'
 
--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,15 @@ Bundler.require(*Rails.groups)
 
 module EwhurstHistory
   class Application < Rails::Application
+    config.middleware.insert_before(Rack::Lock, Rack::Rewrite) do
+      #rewrite   '/wiki/John_Trupiano',  '/john'
+      #r301      '/wiki/Yair_Flicker',   '/yair'
+      #r302      '/wiki/Greg_Jastrab',   '/greg'
+      #r301      %r{/wiki/(\w+)_\w+},    '/$1'
+      r301 "/default.asp", "/"
+      r301 "/v2/default.asp", "/"
+    end
+    #
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
